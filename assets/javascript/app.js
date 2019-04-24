95952d7630e98971ac03eafb470c89adaa408eaf
Erin Lyden <irishjedi77@mac.com>
2019-04-23 22:12:36 -0700

    added style

---
+++ assets/javascript/app.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+
+//Trivia variables
+
+var correct
+var incorrect
+var unanswered
+var timer
+
+//Trivia questions
+
+//q1: Which character in Alice's Adventures in Wonderland was based on the author, Lewis Carroll, himself?
+//q2: Lewis Carroll is a "pen-name." What is the author's real name?
+//q3: Alice's Adventures in Wonderland and Through the Looking Glass were both banned in China. Why?
+//q4: What sea creatures did the Walrus and the Carpenter eat?
+//q5: Which character said "We're all mad here. I'm mad. You're mad.”?
+
+//Trivia options
+
+//o1: ["the Walrus", "the White Rabbit", "the Dodo", "Bill the Lizard"];
+//o2: ["Louis Carroll", "Edgar Cuthwellis", "Charles Lutwidge Dodgson", "Edgar U. C. Westhill"];
+//o3: ["Animals should not use human language", "political bias", "drug references", "inappropriate imagery"];
+//o4: ["clams", "oysters", "prawns", "octopus"];
+//o5: ["the Mad Hatter", "the March Hare", "the Queen of Hearts", "the Cheshire cat"]
+
+//Trivia answers
+
+//a1:"the Dodo"
+//a2:"Charles Lutwidge Dodgson"
+//a3:"Animals should not use human language"
+//a4:"oysters"
+//a5:"the Cheshire cat"
+
+//shows the loading gif when start button is clicked.
+
+    $("#start-button").click(function (){
+        $("#image-holder").show("<img src=https://gph.to/2VkAp9B>");
+        
+    });
+
+    
+});
+
+
+
